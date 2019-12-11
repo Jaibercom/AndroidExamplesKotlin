d6b239f3dfe5a043366ff1e1982c09c439f234d3
YEPEJ010 <yepej010@apps.disney.com>
2019-12-11 18:39:15 -0500

    Adding examples for: Linear and Constraint layout

---
+++ UI-InputControls/constraint/src/main/java/co/edu/udea/compumovil/constraint/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.compumovil.constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ UI-InputControls/linear/src/main/java/co/edu/udea/compumovil/ui_inputcontrols/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.edu.udea.compumovil.ui_inputcontrols
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
