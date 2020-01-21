7139885bd5a893cdadf666d96bc9bcaeefe43137
YEPEJ010 <yepej010@apps.disney.com>
2020-01-21 14:37:01 -0500

    Adding button input control example

---
+++ UI-InputControls/button/src/main/java/co/edu/udea/compumovil/ui_inputcontrols/MainActivity.kt
@@ -0,0 +1,39 @@
+package co.edu.udea.compumovil.ui_inputcontrols
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text_message)
+        updateUI()
+    }
+
+    fun onClick(view: View) {
+
+        when (view.id) {
+
+            R.id.btn_count -> {
+                count++
+            }
+
+            R.id.btn_clear -> {
+                count = 0
+            }
+        }
+        updateUI()
+    }
+
+    private fun updateUI() {
+        textView.text = this.getString(R.string.text_message, count)
+    }
+}
