cfaaa191e1b8363070406682287a953b76d02a73
Jaiber Yepes <jaibercom@gmail.com>
2021-09-23 21:56:27 -0500

    Adding Settings Fragment

--- Navigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ Navigation/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -98,8 +98,7 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
-                        view.findNavController()
-                                .navigate(R.id.action_gameFragment_to_gameWonFragment)
+                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
