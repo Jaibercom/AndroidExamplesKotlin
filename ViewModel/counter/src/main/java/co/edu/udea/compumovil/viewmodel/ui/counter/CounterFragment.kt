41193273ff9911754e4321ef5603c332edc83de6
Jaiber Yepes <jaibercom@gmail.com>
2021-09-28 18:58:43 -0500

    updating project, databinding

--- ViewModel/counter/src/main/java/co/edu/udea/compumovil/viewmodel/ui/counter/CounterFragment.kt
+++ ViewModel/counter/src/main/java/co/edu/udea/compumovil/viewmodel/ui/counter/CounterFragment.kt
@@ -1,37 +1,42 @@
 package co.edu.udea.compumovil.viewmodel.ui.counter
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import co.edu.udea.compumovil.viewmodel.R
+import co.edu.udea.compumovil.viewmodel.databinding.CounterFragmentBinding
 
 class CounterFragment : Fragment() {
 
+    private lateinit var binding: CounterFragmentBinding
     private lateinit var counterViewModel: CounterViewModel
-    private lateinit var addButton: Button
-    private lateinit var clearButton: Button
-    private lateinit var messageView: TextView
-//    private val model: CounterViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.counter_fragment, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.counter_fragment,
+            container,
+            false
+        )
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        addButton = view.findViewById(R.id.add_button)
-        clearButton = view.findViewById(R.id.clear_button)
-        messageView = view.findViewById(R.id.message_view)
-
+        Log.d(TAG, "Called ViewModelProvider.get")
         counterViewModel = ViewModelProvider(this).get(CounterViewModel::class.java)
 
         setUpListeners()
@@ -39,13 +44,13 @@ class CounterFragment : Fragment() {
     }
 
     private fun setUpListeners() {
-        addButton.setOnClickListener { onAdd() }
-        clearButton.setOnClickListener { onClear() }
+        binding.addButton.setOnClickListener { onAdd() }
+        binding.clearButton.setOnClickListener { onClear() }
     }
 
     private fun observeLiveData() {
         counterViewModel.getCounter().observe(viewLifecycleOwner, { newCounter ->
-            messageView.text = requireContext().getString(R.string.text_message, newCounter)
+            binding.messageView.text = requireContext().getString(R.string.text_message, newCounter)
         })
     }
 
@@ -55,5 +60,9 @@ class CounterFragment : Fragment() {
 
     private fun onClear() {
         counterViewModel.onClear()
+    }
+
+    companion object {
+        private const val TAG = "Counter Fragment"
     }
 }
