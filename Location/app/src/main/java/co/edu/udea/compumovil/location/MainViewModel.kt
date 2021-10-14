8b1c5ed0675ae0d05c57186318e801d54ae14d6c
Jaiber Yepes <jaibercom@gmail.com>
2021-10-13 21:55:40 -0500

    updating location example with ViewModel

---
+++ Location/app/src/main/java/co/edu/udea/compumovil/location/MainViewModel.kt
@@ -0,0 +1,111 @@
+package co.edu.udea.compumovil.location
+
+import android.app.Application
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "MainViewModel"
+
+/**
+ * Created by Jaiber Yepes on 13/10/21.
+ */
+class MainViewModel(var mApplication: Application) : AndroidViewModel(mApplication) {
+
+    // FusedLocationProviderClient - Main class for receiving location updates.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // LocationRequest - Requirements for the location updates, i.e., how often you should receive
+    // updates, the priority, etc.
+    private lateinit var locationRequest: LocationRequest
+
+    // LocationCallback - Called when FusedLocationProviderClient has a new Location.
+    private lateinit var locationCallback: LocationCallback
+
+    // Used only for local storage of the last known location. Usually, this would be saved to your
+    // database, but because this is a simplified sample without a full database, we only need the
+    // last location to create a Notification if the user navigates away from the app.
+    private var currentLocation: MutableLiveData<Location?> = MutableLiveData()
+
+    init {
+        initializeLocationProvider()
+    }
+
+    fun getCurrentLocation(): LiveData<Location?> = currentLocation
+
+    private fun initializeLocationProvider() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(mApplication)
+
+        locationRequest = LocationRequest.create().apply {
+            // Sets the desired interval for active location updates. This interval is inexact. You
+            // may not receive updates at all if no location sources are available, or you may
+            // receive them less frequently than requested. You may also receive updates more
+            // frequently than requested if other applications are requesting location at a more
+            // frequent interval.
+            //
+            // IMPORTANT NOTE: Apps running on Android 8.0 and higher devices (regardless of
+            // targetSdkVersion) may receive updates less frequently than this interval when the app
+            // is no longer in the foreground.
+            interval = TimeUnit.SECONDS.toMillis(10)
+
+            // Sets the fastest rate for active location updates. This interval is exact, and your
+            // application will never receive updates more frequently than this value.
+            fastestInterval = TimeUnit.SECONDS.toMillis(5)
+
+            // Sets the maximum time when batched location updates are delivered. Updates may be
+            // delivered sooner than this interval.
+            maxWaitTime = TimeUnit.SECONDS.toMillis(20)
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                // Normally, you want to save a new location to a database. We are simplifying
+                // things a bit and just saving it as a local variable, as we only need it again
+                // if a Notification is created (when the user navigates away from app).
+                currentLocation.value = locationResult.lastLocation
+
+                Log.d(TAG, "New location: {${currentLocation.value.toText()}}")
+            }
+        }
+    }
+
+    fun subscribeToLocationUpdates() {
+        Log.d(TAG, "Subscribe to Location Updates")
+
+        try {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        } catch (unlikely: SecurityException) {
+            Log.e(TAG, "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+
+    fun unsubscribeToLocationUpdates() {
+        Log.d(TAG, "Unsubscribe to Location Updates")
+
+        try {
+            val removeTask = fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            removeTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Location Callback removed.")
+                } else {
+                    Log.d(TAG, "Failed to remove Location Callback.")
+                }
+            }
+        } catch (unlikely: SecurityException) {
+            Log.e(TAG, "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+}
