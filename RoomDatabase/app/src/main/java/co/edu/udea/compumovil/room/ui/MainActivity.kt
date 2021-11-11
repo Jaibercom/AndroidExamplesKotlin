719c5646f4ae38caa9b51ce60b3d81e9f4e1558b
Jaiber Yepes <jaibercom@gmail.com>
2021-11-11 17:50:25 -0500

    update

--- RoomDatabase/app/src/main/java/co/edu/udea/compumovil/room/ui/MainActivity.kt
+++ RoomDatabase/app/src/main/java/co/edu/udea/compumovil/room/ui/MainActivity.kt
@@ -2,17 +2,15 @@ package co.edu.udea.compumovil.room.ui
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.edu.udea.compumovil.room.R
 import co.edu.udea.compumovil.room.adapter.WordListAdapter
 import co.edu.udea.compumovil.room.db.Word
-import co.edu.udea.compumovil.room.db.WordRoomDatabase
 import co.edu.udea.compumovil.room.viewmodel.WordViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -46,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observeLiveData() {
-        wordViewModel.allWords.observe(this, Observer { words ->
+        wordViewModel.allWords.observe(this, { words ->
             // Update the cached copy of the words in the adapter.
             words?.let { adapter.setWords(it) }
         })
